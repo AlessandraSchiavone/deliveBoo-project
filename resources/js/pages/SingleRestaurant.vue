4fbb8473d75611e0cb991cab9de87753cf9e92f4
gfederica <effegiudice@gmail.com>
2021-09-07 19:22:34 +0200

    Menù Ristorante

--- resources/js/pages/SingleRestaurant.vue
+++ resources/js/pages/SingleRestaurant.vue
@@ -1,41 +1,48 @@
 <template>
-  <section v-if="JSON.stringify(restaurant) != '{}'">
+  <section v-if="restaurant">
       <div class="jumbotron">
           <img :src="restaurant.img" :alt="restaurant.name">
       </div>
       <div class="container">
-        <div class="row d-flex justify-content-center align-items-center">
+        <div class="row d-flex justify-content-center">
             <div class="col-md-2">
                 x
             </div>
-            <div class="box col-md-6 text-center py-5">
+            <div class="box col-md-6">
+                <div class="box-top py-5 text-center">
                     <h2>{{restaurant.name}}</h2>
-                    <h5>
-                        <span v-for="cuisine in restaurant.cuisines"
-                        :key="cuisine.id">{{cuisine.name}}</span>
-                    </h5>
-                <h5>
-                        <span v-for="n in restaurant.price_rating"
-                        :key="n">&euro;</span>
-                </h5>
+                    <h6>
+                    <span v-for="cuisine in restaurant.cuisines"
+                    :key="cuisine.id">{{cuisine.name}}  </span>
+                    </h6>
+                    <h6>
+                    <span class="euro" v-for="n in restaurant.price_rating"
+                    :key="n">&euro;</span>
+                    </h6>
                     <h6>{{restaurant.location}}</h6>
                     <div class="consegna">
                         <div class="row d-flex justify-content-center align-items-center">
-                            <div class="col-md-3">
+                            <div class="col-md-6">
                                 <h5>Consegna</h5>
                             </div>
-                            <div class="col-md-3">
+                            <div class="col-md-6">
                                 <h6>{{ restaurant.price_shipping == 0 ? 'Gratis': `${restaurant.price_shipping} &euro;`}}</h6>
                             </div>
                         </div>
-                    
                     </div>
                 </div>
-                <div class="box col-md-3">
-                x</div>  
+                <div v-for="dish in restaurant.dishes"
+                :key="dish.id" class="text-left p-4 menu-card">
+                    <h5>{{dish.name}}</h5>
+                    <p>{{dish.description}}</p>
+                    <h6>{{dish.price}} €</h6>
+                </div>
             </div>
+            <div class="box col-md-3">
+                <div class="box-top">x</div>
+            </div>  
         </div>
-        
+        </div>
 
   </section>
   <section v-else>
@@ -49,13 +56,11 @@ export default {
     data() {
         return {
             restaurant: null,
-            dishes: [],
             categories: []
         }
     },
     created() {
         this.getRestaurant(this.$route.params.slug);
-        // this.getCuisines();
     },
     methods: {
         getRestaurant(slug) {
@@ -86,7 +91,7 @@ export default {
         border-radius: 0;
         margin: 0;
         padding: 0;
-        background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.425), rgba(0, 0, 0, 0));
+        background-image: linear-gradient(to bottom, rgba(255, 255, 255, 1.0), rgba(0, 0, 0, 0), rgba(0,0,0,0));
         background-color: transparent;
          & img {
             width: 100%;
@@ -99,12 +104,54 @@ export default {
         }
 
         .box {
-            background-color: white;
-            border: 1px solid #e2e6e9;
-            margin: -80px 10px auto;
-            h2 {
+            .box-top {
+                background-color: white;
+                margin: -80px 10px 50px;
+                border-radius: 4px;
+                padding: 0;
+
+                h2 {
+                    font-weight: bold;
+                }
+
+                .euro {
+                    color: #F36D00;
+                    font-weight: bold;
+                }
+            }
+
+            .menu-card {
+                background-color: white;
+                border: 1px solid #e2e6e9;
+                margin: 0 10px 10px;
+                h5 {
+                    font-weight: bold;
+                }
+                h6 {
+                    color: #4764CF;
+                }
+            }
+        }
+
+    
+
+
+
+        .consegna {
+            background-color: #F1F2F4;
+            width: 60%;
+            margin: auto;
+            margin-top: 20px;
+            box-shadow: 1px 2px 3px #e2e6e9;
+            padding: 5px 2px;
+            h5, h6 {
+                margin: 0;
+            }
+            h5 {
                 font-weight: bold;
             }
         }
+
     }
+
 </style>
